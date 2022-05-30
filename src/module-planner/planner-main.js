24f704fc1a11443fcef8206f7fdda6a7d7e44f01
Aishwarya-Hariharan-Iyer <95858016+Aishwarya-Hariharan-Iyer@users.noreply.github.com>
2022-05-30 22:35:56 +0800

    Update planner-main.js
    To add SearchBar

--- src/module-planner/planner-main.js
+++ src/module-planner/planner-main.js
@@ -4,39 +4,28 @@ import Box from "./Components/Box/Box";
 import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
 import Avatar from "@mui/material/Avatar";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Typography from '@mui/material/Typography';
-import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from "@mui/material";
+import SearchModBar from "./Components/SearchModBar";
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Input from '@mui/material/Input';
-import { InputAdornment } from "@mui/material";
-
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-const theme = createTheme();
-
+import Autocomplete from '@mui/material/Autocomplete';
+const API_NUSMODS_URL = 'https://api.nusmods.com/2018-2019/moduleInformation.json';
 
 export default function Planner() {
   const [addGradeText, setAddGradeText] = useState("");
-  const [modList, setModList] = useState([]);
   const [Module, setModule] = useState([]);
   const [addModuleText, setAddModuleText] = useState("");
+  const [info, setInfo] = useState('');
+  const [data, setData] = React.useState([]);
+  
+
+  React.useEffect(
+    () => {
+        fetch(API_NUSMODS_URL)
+        .then(res => res.json())
+        .then(d => setData(d))
+    }, []
+);
+
 
   function handleAddModule(event) {
     // React honours default browser behavior and the
@@ -56,70 +45,40 @@ export default function Planner() {
         isComplete: false
       }
     ];
-    const newModList = [
-      ...modList,
-      {
-        description: description,
-      }
-    ];
     setModule(newModule);
-    console.log(newModule);
-    setModList(newModList);
-    console.log(newModList);
+    //console.log(newModule);
   }
-
   return (
     <>
-    <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Typography component="h1" variant="h5">
-              Plan your modules!
-            </Typography>
-          <Box component="form" noValidate sx={{ mt: 1 }}>
+      <div className="Planner">
+        
+        <h1>Plan your modules!</h1>
+
+        <main>
+          <Box>
             <h2>Add Data</h2>
-            <form onSubmit={handleAddModule}>
-              Module Name
-              <label>
-                <Input
-                  style={{ margin: "0 1rem" }}
-                  type="text"
-                  value={addModuleText}
-                  onChange={(event) => {
-                    setAddModuleText(event.target.value);
-                  }}
-                />
-              </label>
-            </form>
+      <Autocomplete
+      disablePortal
+      id="combo-box-demo"
+      options={data}
+      sx={{ width: 300 }}
+      getOptionLabel = {(option) => option.ModuleCode} 
+      autoSelect = {true}
+      renderInput={(params) => <TextField {...params} label={"Module Code"} />}
+      onChange={(event, value) => {
+        setInfo(value); 
+        if(value!==null){
+          console.log('hi');
+          setInfo(value.ModuleCode);
+        } else {
+          console.log('nay');
+        }
+        }}
+      />
             <p> </p>
-            <form onSubmit={handleAddModule}>
-              Input Grade
               <label>
-                <Input
+              Input Grade
+                <input
                   style={{ margin: "0 1rem" }}
                   type="text"
                   value={addGradeText}
@@ -130,9 +89,8 @@ export default function Planner() {
               </label>
               <p> </p>
               <Button variant="contained">
-                <Input type="submit" value="Add" />
+                <input type="submit" value="Add" onClick={()=>{addModule(info, addGradeText); console.log(Module)}}/>
               </Button>
-            </form>
             <p> </p>
           </Box>
 
@@ -158,10 +116,11 @@ export default function Planner() {
             </table>
           </Box>
           <p> </p>
-          </Box>
-        </Grid>
-      </Grid>
-    </ThemeProvider>
+          <div>
+            <Link href="">Head back to main page</Link>
+          </div>
+        </main>
+      </div>
     </>
   );
 } 
