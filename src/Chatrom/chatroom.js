6b69fef2a6312757ea58090cd681b258b0bd6d9c
Richa Motwani <richavm@richas-mbp.lan>
2022-06-27 23:15:12 +0800

    listItems

---
+++ src/Chatrom/chatroom.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { auth } from '../authentication/firebase-config';
+import Buttons from './Buttons';
+import { Box } from '@mui/material';
+
+export default function Chatroom() {
+
+  return (
+    <>
+        <Box>
+            <h3>CHATROOM</h3>
+            <p> </p>
+            <p> This is yet to be implemented!</p>
+        </Box>
+    </>
+  );
+}
